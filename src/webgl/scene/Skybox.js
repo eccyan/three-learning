c204fdb9c05c4ce370119ce95b4fddea77ff8cf9
eccyan <g00.eccyan@gmail.com>
2018-12-23 15:56:22 +0900

    Refacor skybox map urls

--- src/webgl/scene/Skybox.js
+++ src/webgl/scene/Skybox.js
@@ -1,25 +1,28 @@
 import { gui, webgl, assets } from '../../context';
 
+const width = 100;
+const height = 100;
+const depth = 100;
+const mapURLs = [
+  'assets/images/purplenebula-xpos.tga',
+  'assets/images/purplenebula-xneg.tga',
+  'assets/images/purplenebula-ypos.tga',
+  'assets/images/purplenebula-yneg.tga',
+  'assets/images/purplenebula-zpos.tga',
+  'assets/images/purplenebula-zneg.tga',
+];
+
 module.exports = class Skybox extends THREE.Object3D {
 
   constructor () {
     super();
 
-    // Skybox
-	const width = 100;
-	const height = 100;
-	const depth = 100;
     const geometry = new THREE.BoxGeometry( width, height, depth);
 
     const loader = new THREE.TGALoader();
-    const materials = [
-      new THREE.MeshBasicMaterial( { map: loader.load("assets/images/purplenebula-xpos.tga"), side: THREE.BackSide } ),
-      new THREE.MeshBasicMaterial( { map: loader.load("assets/images/purplenebula-xneg.tga"), side: THREE.BackSide } ),
-      new THREE.MeshBasicMaterial( { map: loader.load("assets/images/purplenebula-ypos.tga"), side: THREE.BackSide } ),
-      new THREE.MeshBasicMaterial( { map: loader.load("assets/images/purplenebula-yneg.tga"), side: THREE.BackSide } ),
-      new THREE.MeshBasicMaterial( { map: loader.load("assets/images/purplenebula-zpos.tga"), side: THREE.BackSide } ),
-      new THREE.MeshBasicMaterial( { map: loader.load("assets/images/purplenebula-zneg.tga"), side: THREE.BackSide } ),
-    ];
+    const materials = mapURLs.map((url) => {
+      return new THREE.MeshBasicMaterial( { map: loader.load(url), side: THREE.BackSide } )
+    });
 
     const skybox = new THREE.Mesh( geometry, materials );
     skybox.applyMatrix (new THREE.Matrix4().makeTranslation(0, 0, 0));
